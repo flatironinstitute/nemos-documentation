ab905929f95ccf8a065c5d1c6410e912b443fee8
Flatiron Jenkins <jenkins@flatironinstitute.org>
2026-09-04 14:18:04 +0000

    Generated documentation for branch/main
    jenkins-CCN-nemos-main-1

---
+++ docs/branch/main/_downloads/c6416059608007071d911db61563e161/nemos-basis-OrthExponentialEval-1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from nemos.basis import OrthExponentialEval
+n_basis_funcs = 5
+decay_rates = np.array([0.01, 0.02, 0.03, 0.04, 0.05]) # sample decay rates
+window_size=10
+ortho_basis = OrthExponentialEval(n_basis_funcs, decay_rates=decay_rates)
+sample_points, basis_values = ortho_basis.evaluate_on_grid(100)
+plt.plot(sample_points, basis_values)
+# Expected:
+## [<matplotlib.lines.Line2D object at ...
+plt.show()
